1c1f7a7c98caa0c2380ac4ab7c37b519d8bc4aab
Dilsan15 <dilshaan.sandhu6@gmail.com>
2022-05-26 11:59:49 -0600

    2022-05-26

--- JavaScriptElements/navBar.js
+++ JavaScriptElements/navBar.js
@@ -1,10 +1,18 @@
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-  }
-  
-  /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-  }
+let navState = 0;
+
+function openOrCloseNav() {
+    
+    if (navState % 2 == 0){
+        document.getElementById("mySidenav").style.width = "270px";
+        document.getElementById("main").style.marginLeft = "250px";
+        navState = 1
+    }
+
+    else if (navState % 2 == 1){
+        document.getElementById("mySidenav").style.width = "0";
+        document.getElementById("main").style.marginLeft = "0";
+        navState = 0
+
+    }
+    
+}
