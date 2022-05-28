bf87ea474ebbd48648030067ec2499f5d4a3c7a5
Dilsan15 <dilshaan.sandhu6@gmail.com>
2022-05-27 22:36:31 -0600

    2022-05-27

---
+++ JSElements/navBar.js
@@ -0,0 +1,27 @@
+let navState = 0;
+
+
+
+function openOrCloseNav() {
+    
+    
+
+    if (navState % 2 == 0){
+        
+        
+        document.getElementsByClassName("sideNav")[0].style.width = "20%";
+        document.getElementsByClassName("mainContent")[0].style.marginLeft = "30rem";
+        document.getElementsByClassName("navBarImage")[0].style.marginLeft = "20rem";
+        navState = 1
+    }
+
+    else if (navState % 2 == 1){
+
+        document.getElementsByClassName("sideNav")[0].style.width = "0%";
+        document.getElementsByClassName("mainContent")[0].style.marginLeft = "10rem";
+        document.getElementsByClassName("navBarImage")[0].style.marginLeft = "1rem";
+        navState = 0
+
+    }
+    
+}
