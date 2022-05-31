e92f3787b0e35348d08975544a07f0da6952d36a
Dilsan15 <dilshaan.sandhu6@gmail.com>
2022-05-31 12:13:54 -0600

    2022-05-31

--- JSElements/navBar.js
+++ JSElements/navBar.js
@@ -9,10 +9,14 @@ function openOrCloseNav() {
     if (navState % 2 == 0){
         
         
-        document.getElementsByClassName("sideNav")[0].style.width = "20%";
-        document.getElementsByClassName("mainContent")[0].style.marginLeft = "30rem";
+        document.getElementsByClassName("sideNav")[0].style.width = "19%";
+        document.getElementsByClassName("mainContent")[0].style.marginLeft = "20rem";
+
+        document.getElementsByClassName("navBarImage")[0].style.transform = "rotate(90deg)";
         document.getElementsByClassName("navBarImage")[0].style.marginLeft = "20rem";
+
         document.getElementsByClassName("autoScrollUpButton")[0].style.marginLeft = "20rem";
+        
         navState = 1
     }
 
@@ -20,8 +24,12 @@ function openOrCloseNav() {
 
         document.getElementsByClassName("sideNav")[0].style.width = "0%";
         document.getElementsByClassName("mainContent")[0].style.marginLeft = "14rem";
+        
+        document.getElementsByClassName("navBarImage")[0].style.transform = "rotate(0deg)";
         document.getElementsByClassName("navBarImage")[0].style.marginLeft = "1rem";
+        
         document.getElementsByClassName("autoScrollUpButton")[0].style.marginLeft = "1rem";
+        
         navState = 0
 
     }
